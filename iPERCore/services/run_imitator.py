51b8608895bea2e1f4fb051c969d71904a77eeb5
czj <chenzhongjie@megvii.com>
2021-04-23 14:28:31 +0800

    add feature: random pose with different joints weights

--- iPERCore/services/run_imitator.py
+++ iPERCore/services/run_imitator.py
@@ -14,7 +14,7 @@ from iPERCore.services.options.meta_info import MetaOutput
 from iPERCore.tools.utils.signals.smooth import temporal_smooth_smpls
 from iPERCore.tools.utils.filesio.cv_utils import load_parse, read_cv2_img, normalize_img
 from iPERCore.tools.utils.filesio.persistence import clear_dir
-from iPERCore.tools.utils.multimedia.video import fuse_source_reference_output
+from iPERCore.tools.utils.multimedia.video import fuse_source_reference_output, fuse_src_ref_multi_outputs
 
 
 def get_src_info_for_inference(opt, vid_info):
@@ -60,6 +60,17 @@ def random_affine_smpls(smpls, scale):
     random_pose = np.random.uniform(-scale, scale, (n, 72))
     smpls[:, 3:-10] += random_pose
     return smpls
+
+def random_affine_smpls_with_weights(smpls, w1=0.05, w2=0.15, w3=0.25):
+    inds_l1 = (np.array([0,1,2,3,6,9]).repeat(3).reshape([-1, 3]) * 3 + np.array([0,1,2])).reshape(-1)
+    inds_l2 = (np.array([4,5,12,13,14,15,16,17]).repeat(3).reshape([-1, 3]) * 3 + np.array([0, 1, 2])).reshape(-1)
+
+    random_pose = np.random.uniform(-w3, w3, smpls[:, 3:-10].shape)
+    random_pose[:, inds_l1] = np.random.uniform(-w1, w1, random_pose[:, inds_l1].shape)
+    random_pose[:, inds_l2] = np.random.uniform(-w2, w2, random_pose[:, inds_l2].shape)
+    smpls[:, 3:-10] += random_pose
+    return smpls
+
 
 def imitate(opt):
     """
@@ -167,6 +178,117 @@ def imitate(opt):
     print("Step 3: running imitator done.")
     return all_meta_outputs
 
+def imitate_with_multi_random_pose(opt):
+    """
+
+    Args:
+        opt:
+
+    Returns:
+        all_meta_outputs (list of MetaOutput):
+
+    """
+
+    print("Step 3: running imitator.")
+
+    if opt.ip:
+        from iPERCore.tools.utils.visualizers.visdom_visualizer import VisdomVisualizer
+        visualizer = VisdomVisualizer(env=opt.model_id, ip=opt.ip, port=opt.port)
+    else:
+        visualizer = None
+
+    # set imitator
+    imitator = ModelsFactory.get_by_name("imitator", opt)
+
+    meta_src_proc = opt.meta_data["meta_src"]
+    meta_ref_proc = opt.meta_data["meta_ref"]
+
+    all_meta_outputs = []
+    for i, meta_src in enumerate(meta_src_proc):
+        """
+        meta_input:
+                path: /p300/tpami/neuralAvatar/sources/fange_1/fange_1_ns=2
+                bg_path: /p300/tpami/neuralAvatar/sources/fange_1/IMG_7225.JPG
+                name: fange_1
+        primitives_dir: ../tests/debug/primitives/fange_1
+        processed_dir: ../tests/debug/primitives/fange_1/processed
+        vid_info_path: ../tests/debug/primitives/fange_1/processed/vid_info.pkl
+        """
+        src_proc_info = ProcessInfo(meta_src)
+        src_proc_info.deserialize()
+
+        src_info = src_proc_info.convert_to_src_info(num_source=opt.num_source)
+        src_info_for_inference = get_src_info_for_inference(opt, src_info)
+
+        # 1. personalization
+        imitator.source_setup(
+            src_path=src_info_for_inference["paths"],
+            src_smpl=src_info_for_inference["smpls"],
+            masks=src_info_for_inference["masks"],
+            bg_img=src_info_for_inference["bg"],
+            offsets=src_info_for_inference["offsets"],
+            links_ids=src_info_for_inference["links"],
+            visualizer=visualizer
+        )
+
+        for j, meta_ref in enumerate(meta_ref_proc):
+            """
+            meta_input:
+                path: /p300/tpami/neuralAvatar/references/videos/bantangzhuyi_1.mp4
+                bg_path:
+                name: bantangzhuyi_1
+                audio: /p300/tpami/neuralAvatar/references/videos/bantangzhuyi_1.mp3
+                fps: 30.02
+                pose_fc: 400.0
+                cam_fc: 150.0
+            primitives_dir: ../tests/debug/primitives/bantangzhuyi_1
+            processed_dir: ../tests/debug/primitives/bantangzhuyi_1/processed
+            vid_info_path: ../tests/debug/primitives/bantangzhuyi_1/processed/vid_info.pkl
+            """
+
+            ref_proc_info = ProcessInfo(meta_ref)
+            ref_proc_info.deserialize()
+
+            ref_info = ref_proc_info.convert_to_ref_info()
+            ref_imgs_paths = ref_info["images"]
+            ref_smpls = ref_info["smpls"]
+            multi_out_img_paths = []
+            for k in range(5):
+                if k == 0:
+                    ref_smpls_tmp = ref_smpls
+                else:
+                    ref_smpls_tmp = random_affine_smpls_with_weights(ref_smpls.copy())
+                ref_smpls_tmp = add_hands_params_to_smpl(ref_smpls_tmp, imitator.body_rec.np_hands_mean)
+
+                meta_output = MetaOutput(meta_src, meta_ref)
+
+                # if there are more than 10 frames, then we will use temporal smooth of smpl.
+                if len(ref_smpls_tmp) > 10:
+                    ref_smpls_tmp = temporal_smooth_smpls(ref_smpls_tmp, pose_fc=meta_output.pose_fc, cam_fc=meta_output.cam_fc)
+
+                out_imgs_dir = clear_dir(meta_output.imitation_dir+'_{}'.format(str(k)))
+
+                outputs = imitator.inference(tgt_paths=ref_imgs_paths, tgt_smpls=ref_smpls_tmp,
+                                             cam_strategy=opt.cam_strategy, output_dir=out_imgs_dir,
+                                             visualizer=visualizer, verbose=True)
+                multi_out_img_paths.append(outputs)
+            multi_out_img_paths = [[o[i] for o in multi_out_img_paths] for i in range(len(multi_out_img_paths[0]))]
+            fuse_src_ref_multi_outputs(
+                meta_output.imitation_mp4, src_info_for_inference["paths"],
+                ref_imgs_paths,
+                multi_out_img_paths,
+                # sorted(glob.glob(os.path.join(meta_output.imitation_dir, "pred_*"))),
+                fps=meta_output.fps,
+                image_size=opt.image_size, pool_size=opt.num_workers
+            )
+
+            all_meta_outputs.append(meta_output)
+
+    for meta_output in all_meta_outputs:
+        print(meta_output)
+
+    print("Step 3: running imitator done.")
+    return all_meta_outputs
 
 def run_imitator(opt):
     # 1. prepreocess
@@ -176,7 +298,8 @@ def run_imitator(opt):
         # 2. personalization
         personalize(opt)
         # 3. imitate
-        all_meta_outputs = imitate(opt)
+        # all_meta_outputs = imitate(opt)
+        all_meta_outputs = imitate_with_multi_random_pose(opt)
     else:
         all_meta_outputs = []
 
@@ -188,5 +311,4 @@ if __name__ == "__main__":
 
     OPT = InferenceOptions().parse()
     run_imitator(opt=OPT)
-
 
